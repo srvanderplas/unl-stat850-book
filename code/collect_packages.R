9679ae2e98140409f2a828cadf7bf32ae040a8b1
Susan Vanderplas <srvanderplas@gmail.com>
2020-08-11 17:58:13 -0500

    update packages

--- code/collect_packages.R
+++ code/collect_packages.R
@@ -1,5 +1,12 @@
 # This script collects all R packages used in the bookdown document for installation
-library(tidyverse)
+dep_list <- c("here", "purrr", "stringr", "dplyr")
+install.packages(setdiff(dep_list, installed.packages()))
+
+library(here)
+library(purrr)
+library(stringr)
+library(dplyr)
+
 rmd_files <- list.files(here::here(), ".Rmd$", recursive = T, full.names = T)
 r_files <- list.files(here::here(), ".R$", recursive = T, full.names = T)
 
@@ -7,7 +14,13 @@ all_files <- c(rmd_files, r_files)
 
 all_lines <- purrr::map(all_files, readLines) %>% unlist() %>% str_trim()
 pattern <- "library\\(([A-z0-9]{1,})\\)(.*)$"
+pattern2 <- "([A-z0-9]{1,})::"
 pkgs <- str_detect(all_lines, pattern)
 pkgs <- all_lines[pkgs] %>% str_replace(pattern, "\\1") %>% str_remove("^# ") %>% unique()
 
-writeLines(pkgs, "data/packages")
+pkgs2 <- str_detect(all_lines, pattern2)
+pkgs2 <- all_lines[pkgs2] %>% str_extract(pattern2) %>% str_remove_all("[`[:punct:]]") %>% unique()
+
+all_pkgs <- c(pkgs, pkgs2) %>% unique()
+
+writeLines(all_pkgs, "data/packages")
