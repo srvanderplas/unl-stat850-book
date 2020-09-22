cbb5bb7246d3228340b1d9e1ecad117b7b242313
Susan Vanderplas <srvanderplas@gmail.com>
2020-09-22 16:33:17 -0500

    code to export sql murder tables

---
+++ code/06_export_sql-murder.R
@@ -0,0 +1,13 @@
+library(RSQLite)
+library(tidyverse)
+library(sas7bdat)
+
+
+con <- dbConnect(RSQLite::SQLite(), "data/sql-murder-mystery.db")
+
+dir.create("data/sql-murder")
+purrr::walk(dbListTables(con), function(x) dbReadTable(con, x) %>%
+              write_csv(path = paste0("data/sql-murder/", x, ".csv")))
+
+
+
