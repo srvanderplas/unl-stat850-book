68971917121c61f3a7a6875c5b813e0a82a82af7
Susan Vanderplas <srvanderplas@gmail.com>
2020-09-23 11:19:20 -0500

    try this?

--- code/06_export_sql-murder.R
+++ code/06_export_sql-murder.R
@@ -7,7 +7,7 @@ con <- dbConnect(RSQLite::SQLite(), "data/sql-murder-mystery.db")
 
 dir.create("data/sql-murder")
 purrr::walk(dbListTables(con), function(x) dbReadTable(con, x) %>%
-              mutate(across(where(is.character), ~str_remove(., "\n"))) %>%
+              mutate(across(where(is.character), ~str_replace_all(., "\\s", " ") %>% str_trim())) %>%
               write_csv(path = paste0("data/sql-murder/", x, ".csv")))
 
 
