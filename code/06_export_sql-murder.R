eef418c586399f6d160b39febae26558827f13a5
Susan Vanderplas <srvanderplas@gmail.com>
2020-09-23 11:16:17 -0500

    remove endlines from db bc sas sucks

--- code/06_export_sql-murder.R
+++ code/06_export_sql-murder.R
@@ -7,6 +7,7 @@ con <- dbConnect(RSQLite::SQLite(), "data/sql-murder-mystery.db")
 
 dir.create("data/sql-murder")
 purrr::walk(dbListTables(con), function(x) dbReadTable(con, x) %>%
+              mutate(across(where(is.character), ~str_remove(., "\n"))) %>%
               write_csv(path = paste0("data/sql-murder/", x, ".csv")))
 
 
